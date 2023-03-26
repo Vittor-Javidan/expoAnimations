3a17846aea46d637333a90a07709d052933d60d0
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-03-26 15:50:40 -0300

    [update] Main menu screen
    	- screen elements style changed

--- app/MainMenu/styles.ts
+++ app/MainMenu/styles.ts
@@ -1,17 +1,17 @@
 import { StyleSheet } from "react-native";
 import Colors from "../../Constants/Colors";
-import { E, SIZE } from "../../Constants/Sizes";
+import { E2, SIZE } from "../../Constants/Sizes";
 
 export const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
     },
     menuItemButton: {
-        height: SIZE * E,
+        padding: SIZE / E2,
+        marginTop: SIZE / E2,
         backgroundColor: Colors.lightGray
     },
     pressableArea: {
-        flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
     },
