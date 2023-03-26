13dc8890f9f44335a3c6a43a748d1c7b441eef26
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-03-26 14:40:38 -0300

    [refactor] Navbar Component
    	- removed some unused  animations properties

--- components/Navbar/animations.ts
+++ components/Navbar/animations.ts
@@ -1,16 +1,9 @@
-import { Dimensions } from "react-native";
 import { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
 import { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue } from "react-native-reanimated";
 import Colors from "../../Constants/Colors";
-import { E2, SIZE } from "../../Constants/Sizes";
-
-const { width: DEVICE_WIDTH } = Dimensions.get("screen")
-const BUTTON_WIDTH = SIZE * E2
 
 export function backButtonSlide() {
 
-    const width = useSharedValue<number>(BUTTON_WIDTH)
-    const coordX = useSharedValue<number>(0)
     const backgroundColor = useSharedValue<string>(Colors.lightGray)
     const actionConfirmed = useSharedValue<boolean>(false)
 
@@ -19,11 +12,7 @@ export function backButtonSlide() {
             backgroundColor.value = "#F00"
         },
         onActive: (event) => {
-            if(coordX.value > BUTTON_WIDTH - DEVICE_WIDTH ) {
-                backgroundColor.value = "#F00"
-            } else {
-                backgroundColor.value = "#0F0"
-            }
+            backgroundColor.value = "#0F0"
         },
         onFinish: () => {
             backgroundColor.value = Colors.lightGray
