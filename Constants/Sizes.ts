65f7e474c55f9f09ac435ddf37e5c175fb288b18
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-03-26 14:59:00 -0300

    [removed] Some styles in Navabar componend

--- Constants/Sizes.ts
+++ Constants/Sizes.ts
@@ -1,17 +1,19 @@
-/** Reference UI Size `SIZE = 40`*/
+
+
+/** Reference UI Size `SIZE = 6% of device Height`*/
 export const SIZE = 40
 
 /** Aurea Constant `E = 1.66`*/
-export const E = 1.66
+export const E = 1.6
 
 /** Aurea Constant `E = 1.66` `E2 = E * E`*/
 export const E2 = E * E
 
 /** Aurea Constant `E = 1.66` `E3 = E * E * E`*/
-export const E3 = E * E * E
+export const E3 = E2 * E
 
 /** Aurea Constant `E = 1.66` `E4 = E * E * E * E`*/
-export const E4 = E * E * E * E
+export const E4 = E3 * E
 
 /** Aurea Constant `E = 1.66` `E5 = E * E * E * E * E`*/
-export const E5 = E * E * E * E * E
+export const E5 = E4 * E
--- components/Navbar/styles.ts
+++ components/Navbar/styles.ts
@@ -1,12 +1,11 @@
 import { StyleSheet } from "react-native";
 import Colors from "../../Constants/Colors";
-import { E, E2, E4, SIZE } from "../../Constants/Sizes";
+import { E, E2, SIZE } from "../../Constants/Sizes";
 
 export const styles = StyleSheet.create({
     navbarWithoutButton: {
         justifyContent: 'center',
         alignItems: 'center',
-        height: SIZE * E2,
         backgroundColor: Colors.darkGray,
         borderBottomColor: Colors.lightGray,
         borderBottomWidth: 2
@@ -15,22 +14,17 @@ export const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        height: SIZE * E2,
         backgroundColor: Colors.darkGray,
     },
     title: {
-        paddingLeft: SIZE / E4,
         fontSize: SIZE * E,
         color: Colors.white
     },
     titleWithButton: {
-        paddingLeft: SIZE / E4,
         fontSize: SIZE,
         color: Colors.white
     },
     animatedView: {
-        height: SIZE * E2,
-        width: SIZE * E2,
         backgroundColor: Colors.lightGray,
     },
     icon: {
